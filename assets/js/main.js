3b2d38da56fbabc26491ba58d0b3870d1885e406
Corey Jackson <cjaxsn@gmail.com>
2026-06-27 14:32:03 -0400

    main.js

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+// Automatic Slideshow - change image every 4 seconds
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 4000);    
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+var modal = document.getElementById('ticketModal');
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+const canvas = document.getElementById("cnvPhone");
+const ctx = canvas.getContext("2d");
+ctx.font = "16px Arial";
+ctx.fillText("416-845-1532",0,20);
+
+function generateBrowserNonce() {
+    const array = new Uint8Array(16);
+    window.crypto.getRandomValues(array);
+    return btoa(String.fromCharCode(...array));
+  }
+  
+  //const nonce = generateBrowserNonce();
+  //console.log(nonce); // Example: "dGhpcyBpcyBhIG5vbmNl"
+  
